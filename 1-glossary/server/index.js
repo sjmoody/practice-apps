48a01e4a4effc167fc9e4f2e5abac6882c331191
Steven Moody <smoody07@gmail.com>
2022-09-12 14:04:39 -0700

    Express server with Webpack Babel Hello World React 17 render

--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -1,1 +1,24 @@
 require("dotenv").config();
+const express = require('express')
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname + '/../client/dist'))
+
+app.get('/api', (req, res) => {
+  res.send(`Hello from Express`)
+});
+
+const http = require("http");
+const hostname = '127.0.0.1';
+
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+app.listen(port, () => {
+  console.log(`Express app listening on port ${port}`)
+})
--- 1-glossary/webpack.config.js
+++ 1-glossary/webpack.config.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 /*
   What should go here?  Great question!
@@ -12,4 +13,32 @@ const path = require("path");
   index.html and styles.css to dist folder upon build
 */
 
-module.exports = {};
+module.exports = {
+  entry:  path.join(__dirname, "client/src", "index.jsx"),
+  output: {
+    path: path.resolve(__dirname, "client/dist")
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "client/src", "index.html"),
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.?jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+};
