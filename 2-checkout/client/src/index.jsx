37e45e462543b425b579e5008fbfeb139aa20ae2
Steven Moody <smoody07@gmail.com>
2022-09-16 14:29:38 -0700

    converts index into react component

--- 2-checkout/client/src/index.jsx
+++ 2-checkout/client/src/index.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import { render } from "react-dom";
 
-render(
-  <div>
-    <p>Hello, World!</p>
-    <p>
-      <code>Page Cookie is: {JSON.stringify(document.cookie, undefined, "\t")}</code>
-    </p>
-  </div>,
-  document.getElementById("root")
-);
+class App extends React.Component {
+  render() {
+    if (1===1) {
+      return (
+        <div>
+          <p>Hello, World from the Component!</p>
+          <p>
+          <code>Page Cookie is: {JSON.stringify(document.cookie, undefined, "\t")}</code>
+          </p>
+        </div>
+      );
+    }
+
+
+  }
+
+}
+render(<App />, document.getElementById("root"));
 
 // TODO
 // - Convert this into a React Component
